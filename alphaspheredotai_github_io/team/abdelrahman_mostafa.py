55319e2cedff1647f8f2ad3d4eb634798332d57e
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-09 00:57:11 +0200

    Add initial class definitions for team members

---
+++ alphaspheredotai_github_io/team/abdelrahman_mostafa.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class AbdelrahmanMostafa:
+    """Abdelrahman Mostafa's information."""
---
+++ alphaspheredotai_github_io/team/mohamed_hisham.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class MohamedHisham:
+    """Mohamed Hisham's information."""
