93866abe72f9dba309d3e213aee167443c3276ce
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-09 00:14:02 +0200

    style: add docstrings for index, projects, contact, and team page functions

--- alphaspheredotai_github_io/alphaspheredotai_github_io.py
+++ alphaspheredotai_github_io/alphaspheredotai_github_io.py
@@ -2,31 +2,30 @@
 
 import reflex as rx
 
-from alphaspheredotai_github_io.pages import (
-    contact_page,
-    home_page,
-    projects_page,
-    team_page,
-)
+from alphaspheredotai_github_io.pages import contact_page, home_page, projects_page, team_page
 
 app = rx.App(theme=rx.theme(radius="large", accent_color="gray"))
 
 
 @rx.page(route="/", title="Home Page")
 def index() -> rx.Component:
+    """Home page."""
     return home_page
 
 
 @rx.page(route="/projects", title="Projects")
 def projects() -> rx.Component:
+    """Projects page."""
     return projects_page
 
 
 @rx.page(route="/contact", title="Contact")
 def contact() -> rx.Component:
+    """Contact page."""
     return contact_page
 
 
 @rx.page(route="/team", title="Team")
 def team() -> rx.Component:
+    """Team page."""
     return team_page
