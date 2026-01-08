896c749f1774749da6b1d326e79b2748a1ae24f9
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-08 19:04:58 +0200

    feat: enhance contact page with updated contact items and images

--- alphaspheredotai_github_io/pages/contact.py
+++ alphaspheredotai_github_io/pages/contact.py
@@ -8,42 +8,46 @@ def contact_page() -> rx.Component:
         (
             navbar_icons(),
             rx.center(
-                (
-                    rx.button(
-                        "alphasphere.ai@gmail.com",
-                        size="4",
-                        variant="soft",
-                        on_click=rx.redirect(
+                rx.flex(
+                    (
+                        contact_item(
+                            "Gmail",
                             "mailto:alphasphere.ai@gmail.com",
-                            is_external=True,
+                            "https://pbs.twimg.com/profile_images/1313394640393957378/L0W5hykJ_400x400.jpg",
+                            "alphasphere.ai@gmail.com",
+                        ),
+                        contact_item(
+                            "GitHub",
+                            "https://github.com/AlphaSphereDotAI",
+                            "https://pbs.twimg.com/profile_images/1633247750010830848/8zfRrYjA_400x400.png",
+                        ),
+                        contact_item(
+                            "Kaggle",
+                            "https://kaggle.com/organizations/AlphaSphereDotAI",
+                            "https://pbs.twimg.com/profile_images/1573129499343978498/03a7wgfE_400x400.jpg",
+                        ),
+                        contact_item(
+                            "Hugging Face",
+                            "https://huggingface.co/AlphaSphereDotAI",
+                            "https://pbs.twimg.com/profile_images/1991559933473497089/mbrRS49P_400x400.jpg",
+                        ),
+                        contact_item(
+                            "LinkedIn",
+                            "https://linkedin.com/company/AlphaSphereDotAI",
+                            "https://pbs.twimg.com/profile_images/1661161645857710081/6WtDIesg_400x400.png",
+                        ),
+                        contact_item(
+                            "X",
+                            "https://x.com/AlphaSphereAI",
+                            "https://pbs.twimg.com/profile_images/1955359038532653056/OSHY3ewP_400x400.jpg",
+                            "@AlphaSphereAI",
                         ),
                     ),
-                    rx.divider(),
-                    rx.hstack(
-                        (
-                            contact_item(
-                                "GitHub", "https://github.com/AlphaSphereDotAI"
-                            ),
-                            contact_item(
-                                "Kaggle",
-                                "https://kaggle.com/organizations/AlphaSphereDotAI",
-                            ),
-                            contact_item(
-                                "Hugging Face",
-                                "https://huggingface.co/AlphaSphereDotAI",
-                            ),
-                            contact_item(
-                                "LinkedIn",
-                                "https://linkedin.com/company/AlphaSphereDotAI",
-                            ),
-                            contact_item(
-                                "X", "https://x.com/AlphaSphereAI", "@AlphaSphereAI"
-                            ),
-                        ),
-                        align="center",
-                        justify="center",
-                        spacing="5",
-                    ),
+                    align="center",
+                    justify="center",
+                    spacing="5",
+                    flex_wrap="wrap",
+                    width="100%",
                 ),
                 justify="center",
                 align="center",
