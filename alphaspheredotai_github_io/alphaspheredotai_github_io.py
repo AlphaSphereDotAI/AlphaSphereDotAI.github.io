cd2e0b87d17b2e99ac1ae3ce4ed25f554a43d15f
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-08 16:07:40 +0200

    Add initial app structure

--- alphaspheredotai_github_io/alphaspheredotai_github_io.py
+++ alphaspheredotai_github_io/alphaspheredotai_github_io.py
@@ -2,29 +2,17 @@
 
 import reflex as rx
 
-from rxconfig import config
+from alphaspheredotai_github_io.component import contact_item, navbar_icons
+
+app = rx.App(theme=rx.theme(has_background=True, radius="large", accent_color="gray"))
 
 
-class State(rx.State):
-    """The app state."""
-
-
+@rx.page(route="/", title="Home Page")
 def index() -> rx.Component:
-    # Welcome Page (Index)
     return rx.container(
-        rx.color_mode.button(position="top-right"),
+        navbar_icons(),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
+            (rx.heading("AlphaSphere.AI", size="9", weight="bold"),),
             spacing="5",
             justify="center",
             min_height="85vh",
@@ -32,5 +20,53 @@ def index() -> rx.Component:
     )
 
 
-app = rx.App()
-app.add_page(index)
+@rx.page(route="/projects", title="Projects")
+def projects() -> rx.Component:
+    return rx.container((
+        navbar_icons(),
+        rx.center(
+            rx.heading("This page is under construction", size="5", weight="bold"),
+            justify="center",
+            align="center",
+            direction="column",
+            min_height="85vh",
+        ),
+    ))
+
+
+@rx.page(route="/contact", title="Contact")
+def contact() -> rx.Component:
+    return rx.container((
+        navbar_icons(),
+        rx.center(
+            (
+                rx.button(
+                    "alphasphere.ai@gmail.com",
+                    size="4",
+                    variant="soft",
+                    on_click=rx.redirect(
+                        "mailto:alphasphere.ai@gmail.com",
+                        is_external=True,
+                    ),
+                ),
+                rx.divider(),
+                rx.hstack(
+                    (
+                        contact_item("GitHub", "https://github.com/AlphaSphereDotAI"),
+                        contact_item("Kaggle", "https://kaggle.com/organizations/AlphaSphereDotAI"),
+                        contact_item("Hugging Face", "https://huggingface.co/AlphaSphereDotAI"),
+                        contact_item("LinkedIn", "https://linkedin.com/company/AlphaSphereDotAI"),
+                        contact_item("Twitter", "https://twitter.com/AlphaSphereAI"),
+                    ),
+                    align="center",
+                    justify="center",
+                    spacing="5",
+                ),
+            ),
+            justify="center",
+            align="center",
+            direction="column",
+            min_height="85vh",
+            spacing="5",
+        ),
+    ))
