041a0f60f6de5a5bf8d1803c5fcfe400f27c2038
MH0386 <77013511+MH0386@users.noreply.github.com>
2026-01-09 18:41:19 +0000

    devenv: Apply Fixes

--- rxconfig.py
+++ rxconfig.py
@@ -6,5 +6,6 @@ config = rx.Config(
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ],
-    show_built_with_reflex=False,api_url="https://alphaspheredotai.is-a.dev"
+    show_built_with_reflex=False,
+    api_url="https://alphaspheredotai.is-a.dev",
 )
