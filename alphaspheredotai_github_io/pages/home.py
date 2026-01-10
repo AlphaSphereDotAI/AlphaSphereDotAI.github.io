239216ed8e073add6457b8a458e31962c7c332aa
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-10 16:51:10 +0200

    fix(home): fix home_page component

--- alphaspheredotai_github_io/pages/home.py
+++ alphaspheredotai_github_io/pages/home.py
@@ -5,37 +5,26 @@ from alphaspheredotai_github_io.component import navbar_icons
 
 def home_page() -> rx.Component:
     """Home page component."""
-    return rx.box(
-        rx.container(
-            navbar_icons(),
-            rx.center(
-                rx.hstack(
-                    rx.center(
-                        (
-                            rx.image(
-                                src="https://raw.githubusercontent.com/AlphaSphereDotAI/.github/main/Logos/400x400.jpg",
-                                alt="AlphaSphere.AI Logo",
-                                border_radius="50px",
-                                height="100px",
-                                width="100px",
-                            ),
-                            rx.heading(
-                                "AlphaSphere.AI",
-                                size="9",
-                                weight="bold",
-                            ),
-                        ),
-                        spacing="5",
-                        justify="center",
-                    ),
-                    spacing="5",
-                    justify="center",
+    return rx.container(
+        navbar_icons(),
+        rx.center(
+            (
+                rx.image(
+                    src="https://raw.githubusercontent.com/AlphaSphereDotAI/.github/main/Logos/400x400.jpg",
+                    alt="AlphaSphere.AI Logo",
+                    border_radius="50%",
+                    height="25%",
+                    width="25%",
                 ),
-                spacing="5",
-                justify="center",
-                min_height="85vh",
+                rx.heading("AlphaSphere.AI", size="9", weight="bold"),
             ),
+            spacing="5",
+            justify="center",
+            align="center",
+            direction="column",
+            min_height="85vh",
         ),
+        min_height="85vh",
         background="linear-gradient(#543af5,#1d173d)",
         radius="full",
         width="100%",
