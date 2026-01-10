dd50b0297909dded3d6a53966fd0860f2c95d566
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-10 19:01:20 +0200

    Use local avatars for team members

--- alphaspheredotai_github_io/team/member/__init__.py
+++ alphaspheredotai_github_io/team/member/__init__.py
@@ -9,7 +9,7 @@ from alphaspheredotai_github_io.team.team_member import SocialLinks
 AbdelrahmanAminInfo = AbdelrahmanAmin(
     name="Abdelrahman Amin",
     role="AI Engineer",
-    avatar="https://media.licdn.com/dms/image/v2/D4D03AQGOKFgkjw3q6w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1675625690207?e=1769644800&v=beta&t=hKWmG_WjxbGjZnjy-RUVsd7bc7oJUC2_U6blE2A6ulM",
+    avatar="/AbdelrahmanAmin.jpg",
     bio="",
     social_links=SocialLinks(
         github="https://github.com/Abdelrahman-Amen",
@@ -20,7 +20,7 @@ AbdelrahmanAminInfo = AbdelrahmanAmin(
 AbdelrahmanMostafaInfo = AbdelrahmanMostafa(
     name="Abdelrahman Mostafa",
     role="AI Engineer",
-    avatar="https://media.licdn.com/dms/image/v2/D4D03AQHTYBrI7_QUpQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1708546633318?e=1769644800&v=beta&t=SOPFAtw4aLk1W987EX9oH94QTrnaJJ6jOoArqnl2ubI",
+    avatar="/AbdelrahmanMostafa.jpg",
     bio="",
     social_links=SocialLinks(
         github="https://github.com/Eng-Abdelrahman-Mostafa-Mohamed",
@@ -31,7 +31,7 @@ AbdelrahmanMostafaInfo = AbdelrahmanMostafa(
 MohamedHishamInfo = MohamedHisham(
     name="Mohamed Hisham",
     role="AI Engineer",
-    avatar="https://media.licdn.com/dms/image/v2/D4D03AQGegt3gttArJw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1708547465163?e=1769644800&v=beta&t=AMmMMuSydMrAm82Ugq67gVZeRrKhSE-SjmK653E4grs",
+    avatar="/MohamedHisham.jpg",
     bio="",
     social_links=SocialLinks(
         github="https://github.com/MH0386",
@@ -42,7 +42,7 @@ MohamedHishamInfo = MohamedHisham(
 YousefMohamedInfo = YousefMohamed(
     name="Yousef Mohamed",
     role="AI Engineer",
-    avatar="https://media.licdn.com/dms/image/v2/D5603AQGYxfpsmJGj5w/profile-displayphoto-crop_800_800/B56Zh1154hG0AM-/0/1754323727959?e=1769644800&v=beta&t=sm8hfTG44P07zDRhAj79ipq0aLAf6AIBibJF0HleOJQ",
+    avatar="/YousefMohamed.jpg",
     bio="",
     social_links=SocialLinks(
         github="https://github.com/Youssef-Mohammad",
